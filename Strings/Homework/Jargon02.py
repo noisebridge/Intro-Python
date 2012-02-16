3f5a823c5eb1dc4c0bb8e46b3a7ba995ab12f8e6
Kellan Jacobs <kellankade@gmail.com>
2012-02-15 18:29:45 -0800

    Cleaned up String Homework
    Signed-off-by: Kellan Jacobs <kellankade@gmail.com>

--- Strings/Homework/Jargon02.py
+++ Strings/Homework/Jargon02.py
@@ -1,25 +1,32 @@
 #!/usr/bin/env python
 
 """
-hw_strings_02.py
+jargon02.py
 
-Assignment: Create a program that will prompt the user for a string. Take the
-string input by the user. Output the sting in side a box that is centered on 
-the screen. Use an 80 column screen for this example. Look at the example from
-class on how to draw a box. There are many more ways to solve this problem than
-the way I presented in the example.
+Modify your jargon module from the last lesson. The new version should take a 
+string from the user and center it in a box on the screen
 
-Use the built in len function to determin the lenght of the string.
-Also your box is not limited to using the characters I chose for my box. You 
-might find it easier to use all * characters for example. 
+The box should have at least 10 characters of padding on each side of the screen
+assume the users screen is 80 characters. I copied my solution from the last 
+homework assignment. You should use your code over mine.
+
+Example Run:
+Please enter a string: You never forget your first doctor
+
+          +----------------------------------------------------------+
+          |            You never forget your first Doctor            |
+          +----------------------------------------------------------+
 
 """
 
-def main():
+lenght = int(raw_input("Enter length: "))
+width = int(raw_input("Enter width: "))
+fillChar = raw_input("Enter fill character: ")
+screenWidth = 80
+padding = ' ' * ((screenWidth - lenght) / 2)
+topRow = '+' + '-' * (lenght - 2) + '+' 
+fillRows = '|' + fillChar * (lenght - 2) + '|' + '\n'
 
-
-
-
-
-if __name__ == '__main__':
-    main()
+print topRow
+print fillRows * (width - 2),
+print topRow
--- Strings/Homework/stardate.py
+++ Strings/Homework/stardate.py
@@ -1,50 +1,35 @@
 #!/usr/bin/env python
 
 """
-hw_strings_01.py
+stardate.py
 
-Assignment: Create a program that will prompt the user for a date and convert that date into stardate
+Assignment: Create a program that will prompt the user their birthdate. Take 
+their birthdate and convert it to a stardate using the rules from StarTrek TNG
 
 Rules for computing stardate.
 
-1. Stardate is a number composed of 4 digits followed by a . and then three more digits.
-2. The first four digits are the year the user gave.
-3. The number after the period is the percentage of days that have passed in the year. 
+1. Stardate is a number composed of 4 digits followed by a . and then up to 
+   3 more digits
+2. The first four digits are the year on the gegorian calendar
+3. The number after the period is the percentage of days that have passed in 
+   that year. 
 
-Example Dec 01, 2011 would be converted to stardate 2011.91
-That is computed by using Dec 1, 2011 is the 335th day of the year and this year has 365 days
-So 365/335 = .91 or 91% 
+If for example I was born on June 30, 1985. The stardate would be 1985.49
+As 49% of the year has passed. 
+June 30 was the 181st day of the year
+In 1985 there were 365 days in the year
+181/365 = 0.4958904109589041 or 49.5%
 
-I have added some code to help you compute dates. 
+I have added some code to help you compute the numbers of days in the year.
 
-Secondly to do math with decimals you will have to cast your numbers as floats.
-
-I have included an example.
-
+Example Run:
+Please enter your birthdate in MM/DD/YYYY format: 06/30/1985
+You were born on StarDate 1985.49
+ 
 """
+# inport date from datetime module
+from datetime import date
 
-import datetime
-
-def main():
-    #compute the number of days in a year. In your final product
-    # you will need to use the actual year the user inputs.
-    daysInTheYear = datetime.date(2011,12,31).strftime("%j")
-
-    ''' 
-    Example only This doesnt need to be in your answer.
-    Test with 30 questions and you got 23 correct. You will need to
-    type cast the values as floats so you can get the percentage correct.
-    Then to get the percentage number you should cast it back as a
-    integer. 
-    '''
-    questionsTotal = "30"
-    questionsCorrect = 23
-    testScore = float(questionsCorrect) / float(questionsTotal)
-    testScore = int(testScore * 100)
-    
-
-
-
-
-if __name__ == '__main__':
-    main()
+#compute the number of days in a year. In your final product
+# you will need to use the actual year the user inputs.
+daysInTheYear = date(2011,12,31).strftime("%j")
