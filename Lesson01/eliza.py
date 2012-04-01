2dee99018b58fb04748ddd567bac9b77d6662cad
Kellan Jacobs <kellankade@gmail.com>
2012-04-01 00:07:11 -0700

    Added Answers to Homework for Lesson 01
    Signed-off-by: Kellan Jacobs <kellankade@gmail.com>

---
+++ Lesson01/eliza.py
@@ -0,0 +1,4 @@
+userName = raw_input("Please give me your name: ")
+print "Hi %s, I am Eliza your thearipist." % (userName) 
+print "Please come into my office and have a seat on the couch."
+print "Great now that you are comfortable please tell me what is troubling you:" 
---
+++ Lesson01/jargon.py
@@ -0,0 +1,10 @@
+lenght = int(raw_input("Enter length: "))
+width = int(raw_input("Enter width: "))
+screenWidth = 80
+padding = ' ' * ((screenWidth - lenght) / 2)
+topRow = padding + '+' + '-' * (lenght - 2) + '+' 
+fillRows = padding + '|' + ' ' * (lenght - 2) + '|' + '\n'
+
+print topRow
+print fillRows * (width - 2),
+print topRow
