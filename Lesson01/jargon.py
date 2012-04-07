10cb911805870e9c87b8904678492056e108cae8
Kellan Jacobs <kellankade@gmail.com>
2012-04-07 11:01:01 -0700

    Fixed newline error
    Signed-off-by: Kellan Jacobs <kellankade@gmail.com>

--- Lesson01/jargon.py
+++ Lesson01/jargon.py
@@ -3,8 +3,9 @@ width = int(raw_input("Enter width: "))
 screenWidth = 80
 padding = ' ' * ((screenWidth - lenght) / 2)
 topRow = padding + '+' + '-' * (lenght - 2) + '+' 
-fillRows = padding + '|' + ' ' * (lenght - 2) + '|' + '\n'
+fillRows = padding + '|' + ' ' * (lenght - 2) + '|' 
+
 
 print topRow
-print fillRows * (width - 2),
+print fillRows * (width - 2)
 print topRow
